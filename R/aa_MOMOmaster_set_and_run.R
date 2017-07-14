3f16ab9204df94b13eb2962dadc3dd5c6912d822
Richard White <a@rwhite.no>
2017-07-14 10:44:32 +0200

    Fixing verbosity

--- R/aa_MOMOmaster_set_and_run.R
+++ R/aa_MOMOmaster_set_and_run.R
@@ -175,17 +175,15 @@ RunMoMo <- function(){
   if(opts$verbose) cat("Iterating over age groups:\n")
   MOMOoutput <- analyzeMOMO(MOMOinput, datesISO=opts$datesISO, useAUTOMN=opts$useAUTOMN,
   	USEglm2=opts$USEglm2, compatibility.mode=TRUE, verbose=opts$verbose)
-print(1)
+
   dataExport$toSave <- vector("list",length=length(MOMOoutput))
   for(j in 1:length(dataExport$toSave)){
     dataExport$toSave[[j]] <- MOMOoutput[[j]]$toSave
     MOMOoutput[[j]]$toSave <- NULL
   }
-print(2)
   if(opts$verbose) cat("Joining output... ")
   MOMOjoinedOutput <- joinMOMOoutput(MOMOoutput)
   if(opts$verbose) cat("DONE\n")
-print(3)
 
   if(opts$verbose) cat("Creating MOMO directories and writing all output to disk... ")
   MOMOdirs <- createMOMOdirectories(MOMOoutput, opts$WDIR)
@@ -211,5 +209,5 @@ print(3)
 
   })
 
-  if(opts$verbose) cat("\nCompleted the analysis in "); cat(round(t0[3],1)); cat(" seconds total.\n")
+  if(opts$verbose) cat("\nCompleted the analysis in ",round(t0[3],1)," seconds total.\n")
 }
